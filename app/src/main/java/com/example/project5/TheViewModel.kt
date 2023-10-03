47fa8ea44248a358c14ad7bae83c1cd5d0a43667
asteitz <asteitz@iu.edu>
2023-10-03 18:28:22 -0400

    documentation for fragment page

--- app/src/main/java/com/example/project5/TheViewModel.kt
+++ app/src/main/java/com/example/project5/TheViewModel.kt
@@ -5,5 +5,9 @@ import androidx.lifecycle.ViewModel
 import com.example.project5.databinding.Fragment1Binding
 
 class TheViewModel : ViewModel() {
+     /**
+      * Creating the constructor and initializing the binding for the live data for the ml kit
+      * @returns view
+      */
      val bindingFrag: MutableLiveData<Fragment1Binding> = MutableLiveData()
 }
